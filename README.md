7cabe2d65df0f9c12d26b1d3b76c5aa830872497
isaac953 <isaac.arias07@gmail.com>
2022-11-10 14:39:40 -0600

    PT-02: Completar contenido y diseño del Header

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Proyecto creado a partir del Curso de Frontend Developer Platzi realizado en Ang
 ## 01 Layout (November 08)
 
 - PT-01: **Integrar Bootstrap y creación del Header, Main y Footer**.
+
+## 02 Header (November 09 - November 10)
+
+- PT-02: **Completar contenido y diseño del Header**.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { DropdownComponent } from './components/header/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
