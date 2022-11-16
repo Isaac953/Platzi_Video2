a9f1d2ee3a2fb890bbe77da86e19548bebef1e7b
isaac953 <isaac.arias07@gmail.com>
2022-11-15 19:50:41 -0600

    PT-03: Estilizar Dropdown de manera preliminar y deploy a Github pages

--- src/app/components/header/dropdown/dropdown.component.ts
+++ src/app/components/header/dropdown/dropdown.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.css']
+  styleUrls: ['./dropdown.component.css'],
 })
 export class DropdownComponent implements OnInit {
+  imgProfile = './assets/logo/user_icon.png';
+  profileNavs: any[] = [
+    {
+      option: 'Cuenta',
+      url: '#',
+    },
+    {
+      option: 'Cerrar Sesión',
+      url: '#',
+    },
+    {
+      class: 'dropdown-divider',
+    },
+    {
+      option: 'Registro',
+      url: '#',
+    },
+    {
+      option: 'Iniciar Sesión',
+      url: '#',
+    },
+    {
+      option: 'Error 404',
+      url: '#',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
